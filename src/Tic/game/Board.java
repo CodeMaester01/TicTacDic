09cf69c268ff8b86e809e38ddd093f7975dad363
Liam Orr <liamorr@buffalo.edu>
2018-04-28 18:35:06 -0400

    board equals method added

--- src/Tic/game/Board.java
+++ src/Tic/game/Board.java
@@ -89,4 +89,13 @@ public class Board {
 		}
 		return false;
 	}
+	
+	public boolean equals(Board b) {
+		for(int i = 0; i<3; i++) {
+			for(int j = 0; j<3; j++) {
+				if(board[i][j] != b.getBoard()[i][j])
+					return false;
+			}
+		}return true;
+	}
 }
