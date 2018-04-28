24ef3b82229b8fe17ca5e1cb5b3046b28373263a
Liam Orr <liamorr@buffalo.edu>
2018-04-28 15:49:47 -0400

    updoot

--- src/Tic/game/GameHandler.java
+++ src/Tic/game/GameHandler.java
@@ -5,10 +5,12 @@ public class GameHandler {
 	private TurnHandler turn;
 	private Board board;
 	
-	
-	
 	public GameHandler() {
 		turn = new TurnHandler();
 		board = new Board();
+	}
+	
+	public void startGame() {
+		
 	}
 }
