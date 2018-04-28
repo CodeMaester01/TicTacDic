aa508f54ea692ff546ac815e0bbfd51426fe8b8e
sam21marchant <sam17marchant@gmail.com>
2018-04-28 15:18:30 -0400

    Added count empty spaces

--- src/Tic/game/Board.java
+++ src/Tic/game/Board.java
@@ -20,4 +20,20 @@ public class Board {
 		
 	}
 	
+	/**
+	 * Returns the number of empty spaces on the board. This is used to determine the number of future game states.
+	 * @param b - The board to be analyzed.
+	 * @return - count - the number of empty spaces on the board.
+	 */
+	public int checkEmptySpaces(State[][] b) {
+		int count = 0;
+		for(int r = 0; r < b.length; r++) {
+			for(int c = 0; c < b[r].length; c++) {
+				if(b[r][c] == State.BLANK)
+					count++;
+			}
+		}
+		return count;
+	}
+	
 }
